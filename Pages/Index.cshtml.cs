30415a5f902d9eb1a49210272f783ef346efe42c
BunkerDev-Company <bunkerdev@yandex.ru>
2025-10-10 14:34:56 +0500

    ver 2.0
    ver 2.0

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -7,9 +7,14 @@ namespace ExampleApplication.Pages
 {
     public class IndexModel : PageModel
     {
+        public string MyText = "";
         public void OnGet()
         {
-            
+            int a = 5;
+            int b = 13;
+            int sum = a + b;
+
+            MyText = $"Результат: {sum}";
         }
     }
 }
